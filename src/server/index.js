e9642daf6b029d1a6a4508ac185aa30141db8ca2
Juan Opazo <dopazo178@gmail.com>
2021-12-26 14:00:14 -0500

    third commit

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,13 @@
 const dotenv = require('dotenv')
 dotenv.config()
-var aylien = require('aylien_textapi')
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
-let textapi = new aylien({
-    application_id: process.env.API_ID,
-    application_key: process.env.API_KEY
-})
 
 console.log(textapi);
 
 const baseURL = 'https://api.meaningcloud.com/sentiment-2.1?'
-
-// let meaningcloudapi = new meaningCloud({
-//     application_key: process.env.API_KEY_MEANING_CLOUD
-// })
 
 const app = express()
 
@@ -39,24 +30,8 @@ app.get('/test', function (req, res) {
 })
 
 app.post('/', function(req, res) {
-    console.log(res)
-    return null
     userInput = req.body.input;
     console.log(`You entered: ${userInput}`);
-    const apiURL = `${baseURL}key=${process.env.API_KEY_MEANING_CLOUD}&url=${userInput}&lang=en`
+    const apiURL = `${baseURL}key=${process.env.API_KEY}&url=${userInput}&lang=en`
 
-    // const response = await fetch(apiURL)
-    // const mcData = await response.json()
-    // console.log(mcData)
-    // res.send(mcData)
-    /** server sends only specified data to the client with below codes
-     * const projectData = {
-     *  score_tag : mcData.score_tag,
-     *  agreement : mcData.agreement,
-     *  subjectivity : mcData.subjectivity,
-     *  confidence : mcData.confidence,
-     *  irony : mcData.irony
-     * }
-     * res.send(projectData);
-     * */
 })
