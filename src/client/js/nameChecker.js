61d727522e157b2cf2a62dc9d5c62ada95db54ac
Juan Opazo <dopazo178@gmail.com>
2021-12-27 22:34:06 -0500

    one more correction

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,16 +1,10 @@
 function checkForName(inputText) {
-    console.log("::: Running checkForName :::", inputText);
-    let names = [
-        "Picard",
-        "Janeway",
-        "Kirk",
-        "Archer",
-        "Georgiou"
-    ]
+    const regex = inputText.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
 
-    if(names.includes(inputText)) {
-        alert("Welcome, Captain!")
+    if(regex === null) {
+        return 0;
     }
+    return 1;
 }
 
 export { checkForName }
